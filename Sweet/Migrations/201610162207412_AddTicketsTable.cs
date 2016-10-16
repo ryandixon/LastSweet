426ef9a15735d5ea0a777b10aa574caca4d51698
Ryan Dixon <dixon104@marshall.edu>
2016-10-16 18:16:00 -0400

    First Commit

---
+++ Sweet/Migrations/201610162207412_AddTicketsTable.cs
@@ -0,0 +1,16 @@
+namespace Sweet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTicketsTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Sweet/Models/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sweet.Models
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Resolve Resolve { get; set; }
+
+    }
+    public enum Resolve
+    {
+        Open = 1,
+        Closed
+    }
+
+}
