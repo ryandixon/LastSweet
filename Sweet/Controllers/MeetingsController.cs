eb13f36f2a260084fecf94c540e3455792121b2b
Ryan Dixon <dixon104@marshall.edu>
2016-10-16 22:11:36 -0400

    5th Commit

--- Sweet/Controllers/MeetingsController.cs
+++ Sweet/Controllers/MeetingsController.cs
@@ -1,27 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using Sweet.Models;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using System.Web.Routing;
 
-namespace Sweet.Controllers
+namespace FinalSweet.Controllers
 {
     public class MeetingsController : Controller
     {
-
         private ApplicationDbContext _dbContext;
-
         public MeetingsController()
         {
             _dbContext = new ApplicationDbContext();
         }
-        // GET: Meetings
-        public ActionResult Index()
-        {
-            var meetings = _dbContext.Meetings.ToList();
+        // GET: Appointments
 
-            return View(meetings);
+        public async Task<ActionResult> Index(string str)
+        {
+            var user = User.Identity.GetUserName();
+            if (user.Equals(""))
+            {
+                return RedirectToAction("Index", new RouteValueDictionary(
+                    new { controller = "Home", action = "Home" })
+                );
+            }
+            else
+            {
+                var meetings = from m in _dbContext.Meetings
+                                   select m;
+                if (!string.IsNullOrEmpty(str))
+                {
+                    meetings = meetings.Where(s => s.UserName.Contains(str));
+                }
+                return View(await meetings.ToListAsync());
+            }
+
+
+
+        }
+        public ActionResult New()
+        {
+            return View();
         }
 
         public ActionResult Add(Meeting meeting)
@@ -30,10 +51,48 @@ namespace Sweet.Controllers
             _dbContext.SaveChanges();
             return RedirectToAction("Index");
         }
-
-        public ActionResult New()
+        public ActionResult Edit(int id)
         {
-            return View();
+            var appointment = _dbContext.Meetings.SingleOrDefault(v => v.MeetingId == id);
+
+            if (appointment == null)
+                return HttpNotFound();
+
+            return View(appointment);
+        }
+        [HttpPost]
+        public ActionResult Update(Meeting meeting)
+        {
+            var appointmentInDb = _dbContext.Meetings.SingleOrDefault(v => v.MeetingId == meeting.MeetingId);
+
+            if (appointmentInDb == null)
+                return HttpNotFound();
+
+            appointmentInDb.UserName = meeting.UserName;
+            appointmentInDb.Schedule = meeting.Schedule;
+            appointmentInDb.Time = meeting.Time;
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var appointment = _dbContext.Meetings.SingleOrDefault(v => v.MeetingId == id);
+
+            if (appointment == null)
+                return HttpNotFound();
+
+            return View(appointment);
+        }
+        [HttpPost]
+        public ActionResult DoDelete(int id)
+        {
+            var appointment = _dbContext.Meetings.SingleOrDefault(v => v.MeetingId == id);
+            if (appointment == null)
+                return HttpNotFound();
+            _dbContext.Meetings.Remove(appointment);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- Sweet/Models/Meeting.cs
+++ Sweet/Models/Meeting.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Sweet.Models
 {
     public class Meeting
     {
         public int MeetingId { get; set; }
+        public string UserName { get; set; }
+        [Display(Name = "Schedule")]
+        [DataType(DataType.Date)]
         public DateTime Schedule { get; set; }
+        public string Time { get; set; }
     }
 }
