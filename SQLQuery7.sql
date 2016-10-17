5c4907e6ae7ba37fa8b120c4018d308a6113ac98
Ryan Dixon <dixon104@marshall.edu>
2016-10-16 23:19:08 -0400

    10th Commit

---
+++ SQLQuery7.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Meetings] (
+    [MeetingId] INT           NOT NULL,
+    [UserName]  VARCHAR (MAX) NULL,
+    [Schedule]  DATETIME      NULL,
+    [Time]      VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([MeetingId] ASC)
+);
---
+++ SQLQuery8.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Tickets] (
+    [TicketId]    INT            IDENTITY (1, 1) NOT NULL,
+    [Title]       NVARCHAR (MAX) NULL,
+    [Description] NVARCHAR (MAX) NULL,
+    [Resolve]     INT            NULL,
+    [Meeting]     DATETIME       NULL,
+    CONSTRAINT [PK_dbo.Tickets] PRIMARY KEY CLUSTERED ([TicketId] ASC)
+);
